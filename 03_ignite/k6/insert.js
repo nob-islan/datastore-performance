dc7a8ddf47af6f86d0c02b744bd1cd6991fcd3a8
nob-islan <nob.snail@gmail.com>
2026-04-20 11:40:08 +0900

    Add k6 script

---
+++ 03_ignite/k6/insert.js
@@ -0,0 +1,30 @@
+import http from "k6/http";
+import { sleep, check } from "k6";
+
+export const options = {
+  scenarios: {
+    contacts: {
+      executor: "shared-iterations",
+      vus: 100,
+      iterations: 1000000,
+      maxDuration: "3h",
+    },
+  },
+};
+
+/**
+ * 大量データを作成します。
+ */
+export default function () {
+  // 年齢を乱数生成
+  const age = Math.floor(Math.random() * 100);
+
+  // リクエスト送信
+  const res = http.post(
+    "http://host.docker.internal:8083/api/v1/user",
+    JSON.stringify({ name: "nob", age: age }),
+    {
+      headers: { "Content-Type": "application/json" },
+    },
+  );
+}
